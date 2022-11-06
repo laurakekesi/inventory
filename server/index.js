98d51aa2598e25551a5c3e87db539c6e759fcc88
“Laura <laurakekesi@gmail.com>
2022-11-06 14:03:24 -0500

    Initial Setup

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+require("dotenv").config();
+
+const PORT = 4000;
+
+express()
+
+.use(function(req, res, next) {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  })
+
+
+    .use(morgan("tiny"))
+    .use(express.static("public"))
+    .use(express.json())
+    .use(express.urlencoded({ extended: false }))
+    .use("/", express.static(__dirname + "/"))
+
+
+.get('/', (req, res) => {
+  res.status(200).json({status: 200, message: "Howdy!"})
+})
+
+
+.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+});
