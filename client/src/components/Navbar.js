61cf4828e9b0d7f7bd2da22e5afb5529901ef3a2
“Laura <laurakekesi@gmail.com>
2022-11-18 13:52:04 -0500

    Started nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { TbApple, TbSausage, TbCheese, TbBread, TbMilk, TbGlass, TbCircles, TbHome } from "react-icons/tb";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <Link to="/category/Produce" element>Produce</Link>
-      <Link to="/category/Protein">Protein</Link>
-      <Link to="/category/Dairy">Dairy</Link>
-      <Link to="/category/Grains">Grains</Link>
-      <Link to="/category/Dry">Dry</Link>
-      <Link to="/category/Alcohol">Alcohol</Link>
-      <Link to="/category/Misc">Misc</Link>
-
+      <PageLink to="/"><TbHome/></PageLink>
+      <PageLink to="/category/Produce"><TbApple/></PageLink>
+      <PageLink to="/category/Protein"><TbSausage/></PageLink>
+      <PageLink to="/category/Dairy"><TbCheese/></PageLink>
+      <PageLink to="/category/Grains"><TbBread/></PageLink>
+      <PageLink to="/category/Dry"><TbMilk/></PageLink>
+      <PageLink to="/category/Alcohol"><TbGlass/></PageLink>
+      <PageLink to="/category/Misc"><TbCircles/></PageLink>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
+const PageLink = styled(Link)`
+font-size: 40px;
+text-decoration: none;
+color: black;
 `
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+background-color: rgba(247, 153, 178, 0.4);
+width: 50px;
+`
+
+
 
 export default Navbar
