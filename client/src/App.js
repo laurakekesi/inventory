ac02bae67b3acc9cbf631003871065e043580528
“Laura <laurakekesi@gmail.com>
2022-11-18 12:32:09 -0500

    Background, components, routing created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Globalstyles from "./styles/Globalstyles";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Category from "./components/Category";
+
+
+
 const App = () => {
   return(
-    <>
-    <div>Hello World!</div>
-    </>
-  )
+    <BrowserRouter>
+    <Globalstyles/>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/:category' element={<Category/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
+
 
 export default App;
---
+++ client/src/components/NeedToBuy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NeedToBuy = () => {
+  return (
+    <div>NeedToBuy</div>
+  )
+}
+
+export default NeedToBuy
