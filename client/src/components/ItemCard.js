c385542e775ac295b106bee06da1fad50b28ca04
“Laura <laurakekesi@gmail.com>
2023-03-10 13:50:47 -0500

    Started item update form

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -1,45 +1,50 @@
-import React, { useContext } from 'react';
-import styled, { keyframes } from 'styled-components';
-import ItemCardIcon from './ItemCardIcon';
+import React, { useContext } from "react";
+import styled, { keyframes } from "styled-components";
+import ItemCardIcon from "./ItemCardIcon";
 import { TbTrash } from "react-icons/tb";
-import UpdateModal from './UpdateModal';
-import { Context } from '../Context';
+import UpdateModal from "./UpdateModal";
+import { Context } from "../Context";
 
+const ItemCard = ({ itemName, itemQuantity, itemCategory, itemId }) => {
+  const { setInventoryAction } = useContext(Context);
 
+  //deletes item, used on TrashButton
+  const deleteItem = (e) => {
+    e.preventDefault();
+    fetch(`/api/item/${itemId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(setInventoryAction("Deleted"))
+      .catch((err) => console.log(err));
+  };
 
-const ItemCard = ({itemName, itemQuantity, itemCategory, itemId}) => {
-const {setInventoryAction} = useContext(Context);
-
-//deletes item, used on TrashButton
-const deleteItem = (e) => {
-  e.preventDefault();
-  fetch(`/api/item/${itemId}`, {
-    method: "DELETE",
-  })
-  .then((res)=>res.json())
-  .then(setInventoryAction("Deleted"))
-  .catch((err) => console.log(err))
-};
-
-
-return (
-<Wrapper>
-    <TopDiv>
-        <ItemCardIcon itemCategory={itemCategory}/>
-    </TopDiv>
-    <MiddleDiv>
+  return (
+    <Wrapper>
+      <TopDiv>
+        <ItemCardIcon itemCategory={itemCategory} />
+      </TopDiv>
+      <MiddleDiv>
         <ItemDescriptor>ITEM NAME</ItemDescriptor>
         <ItemInfo>{itemName}</ItemInfo>
         <ItemDescriptor>QUANTITY</ItemDescriptor>
         <ItemInfo>{itemQuantity}</ItemInfo>
-    </MiddleDiv>
-    <BottomDiv>
-        <UpdateModal/>
-        <TrashButton onClick={deleteItem}><TbTrash/></TrashButton>
-    </BottomDiv>
-</Wrapper>
-)
-}
+      </MiddleDiv>
+      <BottomDiv>
+        <UpdateModal
+          itemName={itemName}
+          itemQuantity={itemQuantity}
+          itemCategory={itemCategory}
+          itemId={itemId}
+          key={`item-${itemId}`}
+        />
+        <TrashButton onClick={deleteItem}>
+          <TbTrash />
+        </TrashButton>
+      </BottomDiv>
+    </Wrapper>
+  );
+};
 
 const backgroundChange = keyframes`
 from {
@@ -66,58 +71,61 @@ font-size: 15px;
 to {
 font-size: 17px;
 }
-`
+`;
 
 const TrashButton = styled.button`
-border: none;
-background: none;
-cursor: pointer;
-font-size: 15px;
-margin-top: 5px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  font-size: 15px;
+  margin-top: 5px;
 
-&:hover{
+  &:hover {
     animation: ${fontSizeChange} 0.3s;
     animation-fill-mode: forwards;
-}
-`
+  }
+`;
 
 const ItemDescriptor = styled.div`
-font-size: 17px;
-margin-bottom: 2px;
-font-family: var(--header-font-family);
-color: black;
-`
+  font-size: 17px;
+  margin-bottom: 2px;
+  font-family: var(--header-font-family);
+  color: black;
+`;
 const ItemInfo = styled.div`
-font-size: 17px;
-margin-bottom: 15px;
-color: black;
-`
+  font-size: 17px;
+  margin-bottom: 15px;
+  color: black;
+`;
 const TopDiv = styled.div`
-height: 30%;
-display: flex;
-justify-content: center;
-align-items: center;
-padding-top: 10px;
-`
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+`;
 const MiddleDiv = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 const BottomDiv = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 const Wrapper = styled.div`
-height: 40vh;
-width: 15vw;
-margin: 10px;
-border-radius: 20px;
-box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-background-image: linear-gradient(rgba(250, 179, 197,0.2), rgba(255, 255, 255, 0.2));
-color: grey;
-`
-export default ItemCard
+  height: 40vh;
+  width: 15vw;
+  margin: 10px;
+  border-radius: 20px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  background-image: linear-gradient(
+    rgba(250, 179, 197, 0.2),
+    rgba(255, 255, 255, 0.2)
+  );
+  color: grey;
+`;
+export default ItemCard;
--- client/src/components/UpdateModal.js
+++ client/src/components/UpdateModal.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import styled, {keyframes} from 'styled-components';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import styled, { keyframes } from "styled-components";
 
-const UpdateModal = () => {
+const UpdateModal = ({ itemName, itemQuantity, itemCategory, itemId }) => {
   const [show, setShow] = useState(false);
+  const [itemNameOriginal, setItemName] = useState(itemName);
+  const [itemQuantityOriginal, setItemQuantity] = useState(itemQuantity);
+  const [itemCategoryOriginal, setItemCategory] = useState(itemCategory);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  //sets new Item Category depending on selected value in the form.
+  const itemCatChange = (e) => {
+    setItemCategory(e.target.value);
+  };
 
   return (
     <>
@@ -22,11 +30,38 @@ const UpdateModal = () => {
         keyboard={false}
         centered={true}
       >
-        <Modal.Header closeButton style={{backgroundColor: '#257df9'}}>
-          <Modal.Title style={{color: 'white'}}>Update Item</Modal.Title>
+        <Modal.Header closeButton style={{ backgroundColor: "#257df9" }}>
+          <Modal.Title style={{ color: "white" }}>Update Item</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Update item info
+          <Label for="itemName">Item Name:</Label>
+          <Form>
+            <Input
+              onChange={(e) => setItemName(e.target.value)}
+              placeholder={itemName}
+              id="itemName"
+            ></Input>
+            <Label for="itemQuantity">Item Quantity:</Label>
+            <Input
+              onChange={(e) => setItemQuantity(e.target.value)}
+              placeholder={itemQuantity}
+              id="itemQuantity"
+              type="number"
+            ></Input>
+            <Label for="itemCategory">Item Category:</Label>
+            <select name="itemCategory" id="itemCategory" onChange={itemCatChange}>
+              <option value="" disabled selected hidden>
+                {itemCategory}
+              </option>
+              <option value="Dairy">Dairy</option>
+              <option value="Grains">Grains</option>
+              <option value="Produce">Produce</option>
+              <option value="Alcohol">Alcohol</option>
+              <option value="Protein">Protein</option>
+              <option value="Dry">Dry</option>
+              <option value="Misc">Miscellaneous</option>
+            </select>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -37,7 +72,7 @@ const UpdateModal = () => {
       </Modal>
     </>
   );
-}
+};
 const backgroundChange = keyframes`
 from {
     background-color: rgba(247, 153, 178, 0.2);
@@ -56,21 +91,28 @@ color: white;
 }
 `;
 const UpdateButton = styled.button`
-width: 50%;
-height: 40px;
-background-color: rgba(247, 153, 178, 0.2);
-border-radius: 10px;
-font-family: var(--header-font-family);
-font-size: 16px;
-border: none;
-cursor: pointer;
-/* background-color: beige; */
+  width: 50%;
+  height: 40px;
+  background-color: rgba(247, 153, 178, 0.2);
+  border-radius: 10px;
+  font-family: var(--header-font-family);
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
 
-&:hover{
-  animation: ${backgroundChange} 0.3s,
-    ${fontChange} 0.3s;
+  &:hover {
+    animation: ${backgroundChange} 0.3s, ${fontChange} 0.3s;
     animation-fill-mode: forwards;
-}
-`
+  }
+`;
+
+const Input = styled.input``;
+const Label = styled.label`
+  margin: 5px 0px;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default UpdateModal;
